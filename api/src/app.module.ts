953be54fbf5aa9d9c30c35ae3dfea7f004f92e7b
Egor Maloedov <mr.maloedov@gmail.com>
2026-03-04 21:34:19 +0300

    Добавил ratelimit на контакты

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [ContactModule],
+  imports: [
+    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 5 }]),
+    ContactModule,
+  ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
