c8dba704eeda036279f7dae76d2dc7c9cab36f3b
Egor Maloedov <mr.maloedov@gmail.com>
2026-03-04 21:59:39 +0300

    Фикс linter

--- api/src/contact/contact.service.ts
+++ api/src/contact/contact.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import { ContactDto } from './contact.controller';
 
