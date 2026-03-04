971f42b041ea90f39678cb8e73cb6e46b0c40f69
Egor Maloedov <mr.maloedov@gmail.com>
2026-03-04 17:06:57 +0300

    Add healthcheck

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.getHttpAdapter().get('/health', (_req, res) => res.json({ status: 'ok' }));
   await app.listen(process.env.PORT ?? 3000);
+  console.log('App started on port', process.env.PORT ?? 3000);
 }
-bootstrap().then( () => {
-  console.log('App started');
-})
+bootstrap();
